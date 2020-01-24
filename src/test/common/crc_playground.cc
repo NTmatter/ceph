d1d99c582b81ec69bb1bab1bddca41aa3c7d09e4
Thomas Johnson <thomas.johnson@tbwa.com>
2020-01-24 02:13:23 +0100

    crcs: ceph_crc32c computes expected value.

--- src/test/common/crc_playground.cc
+++ src/test/common/crc_playground.cc
@@ -15,6 +15,31 @@
 #include "common/crc32c_intel_baseline.h"
 #include "common/crc32c_aarch64.h"
 
+// Main preamble for "Hello" frame, with incorrect checksum
+// included computed checksum is tx_crc=82851162,
+// official client returns rx_crc=4080661043
+static __u8 IN_SITU_FAILURE[] = {
+    0x01, // Tag: Hello
+    0x01, // Segment Count: 1
+    
+    0x1c, 0x00, 0x00, 0x00, // Segment 1 length
+    0x00, 0x00, // Segment 1 padding (none)
+    
+    // Segments 2-4, all empty
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    
+    0x00, 0x00, // reserved bytes
+    0x5a, 0x35, 0xf0, 0x04 // checksum
+};
+
 TEST(Crcs, Hello) {
-    std::cout << "This is a test!" << std::endl;
+    ASSERT_EQ(sizeof(IN_SITU_FAILURE), 32u);
+    ASSERT_EQ(sizeof(IN_SITU_FAILURE) - sizeof(uint32_t), 28u);
+}
+
+TEST(Crcs, InSitu) {
+    unsigned val = ceph_crc32c(0, (unsigned char *) IN_SITU_FAILURE, sizeof(IN_SITU_FAILURE) - 4);
+    ASSERT_EQ(4080661043u, val);
 }
